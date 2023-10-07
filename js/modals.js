3e1ba2295c2e66958e6649cc52e6d202ca1b02d6
Steven Simon <stevensimon@Stevens-iMac.local>
2023-10-07 00:40:07 -0500

    Cleaned up JavaScript

--- js/modals.js
+++ js/modals.js
@@ -3,61 +3,10 @@ $(document).ready(function(){
    //Date function
    $('#date').text(new Date().getFullYear());
 
-   var items = new Array();
-   storeid();
-
-   for(var i = 0; i < items.length; i++){
-      myClick(items[i]);
-   }
-
-   //Find all the rows and get their ids
-   function storeid(){
-      var className = document.getElementsByClassName('row');
-      var classnameCount = className.length;
-      for(var j = 0; j < classnameCount; j++){
-          items.push(className[j].id);
-      }
-   }
-
     //Sound
     var pop = $("#pop")[0];
     $('a').click(function(){
        pop.play();
     });
-
-    var el = $(document).scrollTop();
-
-    function myClick(clicker){
-      $('#modal-'+clicker).css('display', 'none');
-      //Hide modals
-
-      $('#'+clicker+'-cta').click(function(){
-         $('#modal-'+clicker).css('z-index', '1000');
-         $('#modal-'+clicker).fadeIn('fast');
-         $('#modal-'+clicker).scrollTop(0);
-         el = $(document).scrollTop();
-         $('body').addClass('my-body-noscroll-class');
-         return false;
-      });//See More
-
-      $('#'+clicker+'-exit').click(function(){
-         $('#modal-'+clicker).fadeOut('fast');
-         $('body').removeClass('my-body-noscroll-class');
-         $(document).scrollTop(el);
-         $('#video-'+clicker).attr('src', $('#video-'+clicker).attr('src'));
-         $('#video-'+clicker).css('display', 'block');
-         return false;
-      });//Close Top
-
-      $('#'+clicker+'-exit-bottom').click(function(){
-         $('#modal-'+clicker).fadeOut('fast');
-         $('body').removeClass('my-body-noscroll-class');
-         $(document).scrollTop(el);
-         $('#video-'+clicker).attr('src', $('#video-'+clicker).attr('src'));
-         $('#video-'+clicker).css('display', 'block');
-         return false;
-      });//Close Bottom
-
-    }//myClick
 
 });//doc ready
