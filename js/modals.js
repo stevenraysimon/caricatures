c3bc6d2b201b56756bf5b293b6692176c164ba04
Steven Simon <stevensimon@Stevens-iMac.local>
2023-11-24 16:15:48 -0600

    Added mobile menu

--- js/modals.js
+++ js/modals.js
@@ -9,4 +9,55 @@ $(document).ready(function(){
        pop.play();
     });
 
+    //Menu
+    $('.menu-icon').click(function(){
+
+      if ($(".mainnav").css("display") == "block" ){
+         //Hide
+         $('#barTwo').css('margin-top', '5px');
+         $('#barOne').css({'transform' : 'rotate('+ 0 +'deg)'});
+         $('#barTwo').css({'transform' : 'rotate('+ 0 +'deg)'});
+         $('.mainnav').animate({ left: '-300px' }, function() {
+            $(this).css('display', 'none');
+        });
+         $('.blackOverlay').fadeOut();
+         $('.bar').removeClass('invert');
+      } else {
+         //Show
+         $('.mainnav').css({'display' : 'block'});
+         $('.blackOverlay').fadeIn();
+         $('#barTwo').css('margin-top', '-2px');
+         $('#barOne').css({'transform' : 'rotate('+ 45 +'deg)'});
+         $('#barTwo').css({'transform' : 'rotate('+ -45 +'deg)'});
+         $('.mainnav').animate({ left: '0px' });
+         $('.bar').addClass('invert');
+      }
+
+    });//Menu button
+
+    $('.mainnav a').click(function(){
+      //Hide
+      $('#barTwo').css('margin-top', '5px');
+      $('#barOne').css({'transform' : 'rotate('+ 0 +'deg)'});
+      $('#barTwo').css({'transform' : 'rotate('+ 0 +'deg)'});
+      $('.mainnav').animate({ left: '-300px' }, function() {
+         $(this).css('display', 'none');
+      });
+      $('.blackOverlay').fadeOut();
+      $('.bar').removeClass('invert');
+    });//Main nav links
+
+    // Smooth scroll to section when clicking on a navigation link
+    $('a[href^="#"]').on('click', function(event) {
+      event.preventDefault();
+
+      var target = $(this.getAttribute('href'));
+
+      if (target.length) {
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top
+        }, 1000);
+      }
+    });
+
 });//doc ready
