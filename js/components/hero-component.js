ad064c7442d3c1191658baf6e4c2889c72677a26
Steven Simon <ssimon@Stevens-MacBook-Pro.local>
2025-03-15 16:10:45 -0500

    Added new favicons and right sizes

--- js/components/hero-component.js
+++ js/components/hero-component.js
@@ -14,8 +14,8 @@ class HeroComponent extends HTMLElement {
                                 <h1 class="hiddenTags">Caricatures by Steven Simon</h1>
                                 <a href="https://stevenraysimon.github.io/caricatures/">
                                     <picture>
-                                        <source srcset="https://stevenraysimon.github.io/caricatures/images/logo.webp" type="image/webp">
-                                        <img src="https://stevenraysimon.github.io/caricatures/images/logo.png" title="Caricatures by Steven Simon" alt="Caricatures by Steven Simon" class="logo">
+                                        <source srcset="https://stevenraysimon.github.io/caricatures/images/logo.webp" type="image/webp" sizes="(max-width: 600px) 80vw, 250px" width="766" height="217">
+                                        <img src="https://stevenraysimon.github.io/caricatures/images/logo.png" alt="Caricatures by Steven Simon" title="Caricatures by Steven Simon" class="logo" style="max-width: 250px; width: 100%; height: auto;" width="766" height="217">
                                     </picture>
                                 </a>
                                 <div class="inside-wrapper">
