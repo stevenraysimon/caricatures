5a542a0d320de29f13d6eb3428aaf8dcab2b1a56
Steven Simon <ssimon@Stevens-MacBook-Pro.local>
2025-03-25 20:12:24 -0500

    Fixed caricature card component to load within so loads fast

--- js/components/caricature-card-component.js
+++ js/components/caricature-card-component.js
@@ -10,16 +10,14 @@ class CaricatureCard extends HTMLElement {
         const webpSrc = this.getAttribute('webp-src') || '';
         const jpgSrc = this.getAttribute('jpg-src') || '';
 
-        // Render the HTML structure
+        // Render the HTML structure without the caricature div
         this.innerHTML = `
-        <div class="caricature">
           <picture>
             <source srcset="${webpSrc}" type="image/webp" sizes="(max-width: 600px) 100vw, 50vw" width="400" height="450">
             <img src="${jpgSrc}" alt="${name}" title="${name}" width="400" height="450" style="max-width: 100%; height: auto;">
           </picture>
           <p class="text">${name}</p>
-        </div>
-      `;
+        `;
     }
 }
 
