87f9472bf822dcfe454d1005fe4719b5e5b69989
Steven Simon <stevenraysimon@yahoo.com>
2026-07-07 02:51:40 -0500

    Created analytics.js

---
+++ js/analytics.js
@@ -0,0 +1,14 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    window.dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+
+gtag('config', 'G-YHQB2SVCS0');
+
+const script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-YHQB2SVCS0';
+document.head.appendChild(script);
