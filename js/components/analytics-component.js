3e499cb30f99a304c1178329ecb235bf96d782a7
Steven Simon <stevenraysimon@yahoo.com>
2026-07-07 02:44:42 -0500

    Updated analytics-component.js

--- js/components/analytics-component.js
+++ js/components/analytics-component.js
@@ -1,19 +1,22 @@
 // js/components/analytics-component.js
 class AnalyticsComponent extends HTMLElement {
     connectedCallback() {
-        // Load gtag script
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            window.dataLayer.push(arguments);
+        }
+
+        window.gtag = gtag;
+
+        gtag('js', new Date());
+        gtag('config', 'G-YHQB2SVCS0'); //stevensimon.art
+        gtag('config', 'G-86V1CSS5RL'); //stevenraysimon.github.io/caricatures
+
         const script = document.createElement('script');
         script.async = true;
         script.src = 'https://www.googletagmanager.com/gtag/js?id=G-YHQB2SVCS0';
         document.head.appendChild(script);
-
-        // Initialize
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){ window.dataLayer.push(arguments); }
-        window.gtag = gtag;
-        gtag('js', new Date());
-        gtag('config', 'G-YHQB2SVCS0'); //stevensimon.art
-        gtag('config', 'G-86V1CSS5RL'); //stevenraysimon.github.io/caricatures
     }
 }
 
