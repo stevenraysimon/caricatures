f32d153f619b98ac8666c6904d3c4da01c7466c1
Steven Simon <stevenraysimon@yahoo.com>
2026-07-07 12:10:26 -0500

    Updated analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,12 +1,15 @@
 window.dataLayer = window.dataLayer || [];
 
 function gtag() {
-    window.dataLayer.push(arguments);
+  dataLayer.push(arguments);
 }
+
+window.gtag = gtag;
 
 gtag('js', new Date());
 
 gtag('config', 'G-YHQB2SVCS0'); //stevensimon.art
+gtag('config', 'G-86V1CSS5RL'); //stevenraysimon.github.io/caricatures
 
 const script = document.createElement('script');
 script.async = true;
